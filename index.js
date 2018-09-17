6d626bb036f75c529b1e8329008ac78f800165ea
bernardopiane <bernardopiane@gmail.com>
2018-09-17 01:28:00 -0300

    trocando pagina do git

---
+++ index.js
@@ -0,0 +1,113 @@
+// function SetNavTop() {
+//     $('.navbar').hide().removeClass("fixed-bottom").addClass("fixed-top").show();
+// }
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunction()
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+        $(".section").addClass("fixScroll");
+    } else {
+        navbar.classList.remove("sticky");
+        $(".section").removeClass("fixScroll");
+    }
+}
+
+$("#homeLink").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#greeting").offset().top
+    }, 500);
+});
+
+$("#portLink").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#port").offset().top
+    }, 500);
+});
+
+$("#aboutLink").click(function () {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top
+    }, 500);
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function (event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+$(function () {
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    });
+});
+
+var TxtRotate = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtRotate.prototype.tick = function () {
+    var i = this.loopNum % this.toRotate.length;
+    var fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    var that = this;
+    var delta = 150 - Math.random() * 100;
+
+    if (this.isDeleting) {
+        delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 200;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+window.onload = function () {
+    var elements = document.getElementsByClassName('txt-rotate');
+    for (var i = 0; i < elements.length; i++) {
+        var toRotate = elements[i].getAttribute('data-rotate');
+        var period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtRotate(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+    // INJECT CSS
+    var css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
+    document.body.appendChild(css);
+};
